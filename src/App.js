ff8ebaaedba1fa74e362c35b0ea191d7a13b2f4e
ArturRdix <rdixlovecode@gmail.com>
2023-08-15 14:45:53 +0300

    Добавление Компонента footer и header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+    <header>
+        <h1 className="logo">Monika Lingerie</h1>
+        <div className="banner"></div>
+    </header>
+    )
+}
