489088fd416ee0579bcdabd09b42cfe3d0ac518d
ArturRdix <rdixlovecode@gmail.com>
2023-08-19 20:00:20 +0300

    Добавлена возможность увидеть итоговый прайс в корзине

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BsFillCartFill } from 'react-icons/bs'
 import OrderItem from './OrderItem';
 
 export default function Header({ orders, onRemove }) {
     const [cartState, setCartState] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(0)
+
+
+    useEffect(() => {
+        let calculatedTotalPrice = 0;
+
+        for (const element of orders) {
+            const price = parseFloat(element.price);
+            if (!isNaN(price)) {
+                calculatedTotalPrice += price;
+            }
+        }
+
+        setTotalPrice(calculatedTotalPrice);
+    }, [orders]);
 
     return (
-        <header>
+        <header>.
             <div className="top-block">
                 <h1 className="logo">Monika Lingerie</h1>
                 <nav>
@@ -18,7 +33,7 @@ export default function Header({ orders, onRemove }) {
                         className={`cart-icon ${cartState && 'active'}`} />
                 </nav>
 
-                {cartState && (
+                {(cartState && (orders.length > 0)) && (
                     <div className="window-cart">
                         {orders.map(element => (
                             <OrderItem
@@ -26,6 +41,7 @@ export default function Header({ orders, onRemove }) {
                                 key={element.id}
                                 item={element} />
                         ))}
+                        <b className='total-price-text'>Итоговая сумма к оплате: {totalPrice} грн.</b>
                     </div>
                 )}
             </div>
